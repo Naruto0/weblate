0661a37f74f76c61ad3195d50f921d23791f7294
Filip Hron <filip.hron@gmail.com>
2018-11-28 21:47:52 +0100

    updates

--- daily_digest.py
+++ daily_digest.py
@@ -1,12 +1,11 @@
 from datetime import datetime, timedelta
 
-from weblate.trans.models import Change, Project
-from weblate.auth.models import User
+from weblate.trans.models import Change
 from weblate.accounts.models import Profile
 
-from pprint import pprint
 
-MAP = {
+# map <change.action> to <Profile> model subscription field
+SUBSCRIPTION_MAP = {
     'subscribe_new_suggestion': Change.ACTION_SUGGESTION,
     'subscribe_new_comment': Change.ACTION_COMMENT,
     'subscribe_any_translation': Change.ACTION_NEW,
@@ -15,18 +14,24 @@ MAP = {
 }
 
 
+# define date for chage extracion
 YESTERDAY = datetime.now().date() - timedelta(days = 1)
 TODAY = datetime.now().date()
 
+
+# process daily digest
 def process():
     digest_profiles = Profile.objects.subscribed_only_digest()
     for profile in digest_profiles:
         profile_sub_proj = profile.subscriptions.all()
         profile_subs = []
+        email = []
 
+        # use the above mapping for filtering the changes regarding this profile
+        # subscriptions
         for _ in profile.get_active_subs():
             try:
-                profile_subs.append(MAP[_])
+                profile_subs.append(SUBSCRIPTION_MAP[_])
             except KeyError:
                 pass
 
@@ -47,6 +52,10 @@ def process():
                 project_id=sub_project,
                 action__in=profile_subs
             )
+
+            if today_changes:
+                email.append(Project)
+
             for change in today_changes.all():
                 print change
 
